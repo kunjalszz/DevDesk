09554706b19eb17a85e232f471d7ba12eb9bf5fc
Kunjal Singh <kunjal2803Agmail.com>
2026-07-27 21:47:08 +0530

    refactor: organize project structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,10 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
-import heroImg from './assets/hero.png'
-import './App.css'
-
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-        <div>
-      <h1>Welcome to DevDesk 🚀</h1>
-      <p>This is my first React project.</p>
-    </div>
-    </>
-  )
+    <main>
+      <h1>DevDesk</h1>
+      <p>Developer Productivity Workspace</p>
+    </main>
+  );
 }
 
-export default App
+export default App;
